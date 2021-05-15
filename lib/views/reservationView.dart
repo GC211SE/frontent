2172310f283fe223b1bea57f88bc3e263153da11
zlaepek <69634389+zlaepek@users.noreply.github.com>
2021-05-15 22:55:14 +0900

    ? null 잘 모르겠음 flutter_date_pickers가 있더라

--- lib/views/reservationView.dart
+++ lib/views/reservationView.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gcrs/Widget/timetable.dart';
+
 import 'package:gcrs/utils/GlobalVariables.dart';
 
 var thisContext;
@@ -40,7 +41,7 @@ class _ReservationViewState extends State<ReservationView> {
               alignment: Alignment.bottomRight,
               child: FloatingActionButton.extended(
                 heroTag: "calendar",
-                onPressed: (){}, //TODO: add onPress make calendar
+                onPressed: (){calendarListDialog();}, //TODO: add onPress make calendar
                 icon: Icon(Icons.calendar_today_outlined),
                 label: Text("Calendar") ),
             ),),
@@ -104,6 +105,41 @@ Future<void> classroomListDialog() async {
                 ),
 
                 Text('This is a demo alert dialog.'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("확인"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      });
+}
+
+// TODO: get calendar list and popup
+/* calendar dialog 띄움 */
+Future<void> calendarListDialog() async {
+  // TODO: 건물 이름으로 list 받아와서 넣기
+  return showDialog<void>(
+    // TODO: get context 찾기 -> 이거 이렇게 하는거 아닌것 같은데 다른 방법 있는것 같은데 모르겠음
+      context: thisContext,
+      barrierDismissible: false, //barrierDismissible - Dialog를 제외한 다른 화면 터치 x
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder( // RoundedRectangleBorder - Dialog 화면 모서리 둥글게 조절
+              borderRadius: BorderRadius.circular(10.0)),
+          //Dialog Main Title
+          title: Column(children: <Widget>[new Text("Calendar"),],),
+          content: SingleChildScrollView(
+            child : ListBody(
+              children: <Widget>[
+
+
+                Text('This is week picker.'),
               ],
             ),
           ),
