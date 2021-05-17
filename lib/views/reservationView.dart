df1327ff51b5d3658bc659ec383c5fbb11e3db1c
zlaepek <69634389+zlaepek@users.noreply.github.com>
2021-05-18 01:52:25 +0900

    cell seperate를 위한 time slice
    cell seperate를 위한 time slice

--- lib/views/reservationView.dart
+++ lib/views/reservationView.dart
@@ -7,8 +7,6 @@ var thisContext;
 // https://gcse.doky.space/api/schedule?bd=IT대학&crn=304
 // TODO: make class to save lecture
 class Lecture {
-
-
   String date; // 요일
   String time; // 시간 -> 밑에 MAP 으로 시간 알아봅시다
 
@@ -17,25 +15,64 @@ class Lecture {
     this.time
   });
 
-  //@override
-  //_CellState createState() => _CellState();
-
   timeCalculator(int time){
-    String startTime = convertToActualTime[time][0];
-    String endTime = convertToActualTime[time][1];
+    // split nth 교시 to hour and minute
+    int startHour = int.parse(convertToActualTime[time][0].substring(0,2));
+    int startMinute = int.parse(convertToActualTime[time][0].substring(2,4));
+    int endHour = int.parse(convertToActualTime[time][1].substring(0,2));
+    int endMinute = int.parse(convertToActualTime[time][1].substring(2,4));
+    int height = 60;
 
-    /// 시간 계산
-    // TODO: 만약 시간 차가 1이라면 2개의 셀에 시간표를 그려줘야함  -> list에 2개의 시간대
-    // TODO: 아니라면 1개의 셀을 잘라서 쓰면 됨 -> list에 1개의 시간대
-    
+    Map<int, List<int>> hourSplit = {};
+    /// 시간 계산 ----------------> 끝
+    /// 만약 시간 차가 1이라면 2개의 셀에 시간표를 그려줘야함  -> list에 2개의 시간대
+    /// 아니라면 1개의 셀을 잘라서 쓰면 됨 -> list에 1개의 시간대
     /// 분 계산
-    // TODO: list (hour index(height, bool) * 2) -> true 면 height 만큼 칠해주고 false 면 안칠해주기, hour index에 해당하는 시간에
-    
+    /// list (hour index(height, bool) * 2) -> true 면 height 만큼 칠해주고 false 면 안칠해주기, hour index에 해당하는 시간에
+    if (startHour == endHour){
+      if (startMinute == 0){ // 앞에 붙은 1, 0
+        hourSplit.addAll({startHour:[
+          startMinute, 1,
+          60 - endMinute, 0]});
+      }
+      else if (endMinute < 60){ // 0, 가운데 낀 1, 0
+        hourSplit.addAll({startHour:[
+          startMinute, 0,
+          60 - endMinute - startMinute, 1,
+          60 - endMinute, 0]});
+      }
+      else { // 0, 뒤에 붙은 1
+        hourSplit.addAll({startHour:[
+          endMinute, 0,
+          60 - endMinute, 1]});
+      }
+    }
+    else{ // (0,1), (1,0)
+      if (endMinute != 0){
+        hourSplit.addAll({startHour:[
+          startMinute, 0,
+          60 - startMinute, 1]});
+        hourSplit.addAll({endHour:[
+          endMinute, 1,
+          60 - endMinute, 0]});
+      }
+      else { // (0,1),
+        hourSplit.addAll({startHour:[
+          startMinute, 0,
+          60 - startMinute, 1]});
+      }
+      // hourSplit.addAll({startHour:[1,1]});
+      // 60 - startMinute;
+      // endMinute;
+    }
+    return hourSplit;
+
     /// cell 에 받은거 만큼 다른색으로 칠해주는 함수 추가 요망
   }
+
   // format = name:[start time, end time]
   static final Map<String, List<String>> convertToActualTime = {
-    '1': ["9:00", "9:50"],
+    '1': ["09:00", "09:50"],
     '2': ["10:00", "10:50"],
     '3': ["11:00", "11:50"],
     '4': ["12:00", "12:50"],
@@ -43,14 +80,14 @@ class Lecture {
     '6': ["14:00", "14:50"],
     '7': ["15:00", "15:50"],
     '8': ["16:00", "16:50"],
-    '9': ["17:30", "8:20"],
+    '9': ["17:30", "18:20"],
     '10': ["18:25", "19:15"],
     '11': ["19:20", "20:10"],
     '12': ["20:15", "21:05"],
     '13': ["21:10", "22:00"],
     '14': ["22:05", "22:55"],
 
-    '21': ["9:30", "10:45"],
+    '21': ["09:30", "10:45"],
     '22': ["11:00", "12:15"],
     '23': ["13:00", "14:15"],
     '24': ["14:30", "15:45"],
